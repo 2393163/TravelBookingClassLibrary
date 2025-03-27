146995811625c64404fec54d11d64b8dd258815d
2392936 <KamiReddy.Pavani@cognizant.com>
2025-03-27 12:30:49 +0530

    Add files via upload

---
+++ TravelBookingClassLibrary-main/Data/PackageContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace travelpackage
+{
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Package> Packages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LTIN593446;Initial Catalog=travelpackage;Integrated Security=True;TrustServerCertificate=true");
+        }
+    }
+
+}
