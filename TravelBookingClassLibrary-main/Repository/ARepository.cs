57b4e92dd1d9230540f25ec3167e510e1b0cba6d
2392823 <Sabura.M@cognizant.com>
2025-03-27 12:55:08 +0530

    Add files via upload

---
+++ TravelBookingClassLibrary-main/Repository/ARepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelBookingClassLibrary.Class;
+
+namespace TravelBookingClassLibrary.Repository
+{
+    public class ARepository
+    {
+        public void AddUsers(Assistance newAsistanceRequest)
+        {
+
+            using (var context = new InsuranceandAssistanceContext())
+
+            {
+                context.Assistances.Add(newAsistanceRequest);
+
+                context.SaveChanges();
+
+            }
+
+        }
+
+        public List<Assistance> GetAllAssistanceRequest()
+
+        {
+
+            using (var context = new InsuranceandAssistanceContext())
+
+            {
+                var assistanceRequest = context.Assistances.ToList();
+                return assistanceRequest;
+
+
+            }
+        }
+        public void UpdateAssistanceRequest(int RequestID, string IssueDescription)
+        {
+            using (var dbContext = new InsuranceandAssistanceContext())
+            {
+                var AssistanceRequest = dbContext.Assistances.Find(RequestID); // Find the insurance record by ID
+                if (AssistanceRequest != null)
+                {
+                    AssistanceRequest.RequestID = RequestID;
+
+                    AssistanceRequest.IssueDescription = IssueDescription;
+
+                    dbContext.SaveChanges();
+                }
+            }
+
+        }
+        public void DeleteAssistanceRequest(int RequestID)
+        {
+            using (var dbContext = new InsuranceandAssistanceContext())
+            {
+                var AssistanceRequest = dbContext.Assistances.Find(RequestID); // Find the insurance record by ID
+                if (AssistanceRequest != null)
+                {
+                    dbContext.Assistances.Remove(AssistanceRequest); // Remove the record
+                    dbContext.SaveChanges(); // Save changes to the database
+                }
+            }
+        }
+    }
+}
