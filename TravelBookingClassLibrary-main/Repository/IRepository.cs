654b9c6bc61451b01496198a977d63bc7513e885
2392823 <Sabura.M@cognizant.com>
2025-03-27 12:55:59 +0530

    Add files via upload

---
+++ TravelBookingClassLibrary-main/Repository/IRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelBookingClassLibrary.Class;
+
+namespace TravelBookingClassLibrary.Repository
+{
+    public class IRepository
+    {
+        public void AddUsers(Insurance newInsurance)
+
+        {
+
+            using (var context = new InsuranceandAssistanceContext())
+
+            {
+
+                context.Insurances.Add(newInsurance);
+
+                context.SaveChanges();
+
+            }
+
+        }
+
+        public List<Insurance> GetAllInsurances()
+
+        {
+
+            using (var context = new InsuranceandAssistanceContext())
+
+            {
+
+                var insurances = context.Insurances.ToList();
+
+                return insurances;
+
+            }
+        }
+        public void UpdateInsurance(int InsuranceID, string CoverageDetails, DateTime PurchaseDate)
+        {
+            using (var dbContext = new InsuranceandAssistanceContext())
+            {
+                var insurance = dbContext.Insurances.Find(InsuranceID); // Find the insurance record by ID
+                if (insurance != null)
+                {
+
+                    insurance.CoverageDetails = CoverageDetails;
+                    insurance.PurchaseDate = PurchaseDate;
+                    dbContext.SaveChanges();
+                }
+            }
+
+        }
+        public void DeleteInsurance(int InsuranceID)
+        {
+            using (var dbContext = new InsuranceandAssistanceContext())
+            {
+                var insurance = dbContext.Insurances.Find(InsuranceID); // Find the insurance record by ID
+                if (insurance != null)
+                {
+                    dbContext.Insurances.Remove(insurance); // Remove the record
+                    dbContext.SaveChanges(); // Save changes to the database
+                }
+            }
+        }
+    }
+}
