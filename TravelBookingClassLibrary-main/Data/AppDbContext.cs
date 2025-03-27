9303d8ebfed99ad1aeec4a75f3a5ff69d981effe
2392823 <Sabura.M@cognizant.com>
2025-03-27 12:50:29 +0530

    Add files via upload

--- TravelBookingClassLibrary-main/Data/AppDbContext.cs
+++ TravelBookingClassLibrary-main/Data/AppDbContext.cs
@@ -1,27 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TravelBookingClassLibrary.Entities;
-using Microsoft.Extensions.Configuration;
-
+using Microsoft.EntityFrameworkCore;
+using TravelBookingClassLibrary.Class;
 namespace TravelBookingClassLibrary.Data
 {
-    public class AppDbContext : DbContext
+    public class InsuranceandAssistanceContext : DbContext
     {
-       // private readonly IConfiguration _configuration;
-      
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if(!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("Data Source=localhost;User=sa;Password=root;Initial Catalog=TravelBookingDB;Integrated Security=True;TrustServerCertificate=true");
-
-            }
+            optionsBuilder.UseSqlServer("Data Source= LTIN593162;Initial Catalog=TravelBookingDb; Integrated Security =true; TrustServerCertificate=true");
         }
-
-        public DbSet<User> Users { get; set; }
+        public DbSet<Insurance> Insurances { get; set; }
+        public DbSet<Assistance> Assistances { get; set; }
     }
 }
