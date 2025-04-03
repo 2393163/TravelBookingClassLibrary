0febf7c56cb45e54c97a1d1259b5e5fee7ab395d
UnyalaSanjana <Unyala.Sanjana@cognizant.com>
2025-04-03 21:16:18 +0530

    Add files via upload

--- TravelBookingClassLibrary-main/Entities/Review.cs
+++ TravelBookingClassLibrary-main/Entities/Review.cs
@@ -1,13 +1,19 @@
-﻿
-namespace TravelBookingSystem
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBookingClassLibrary.Entity
 {
     public class Review
     {
         public int ReviewID { get; set; }
-        public int UserID {  get; set; }
-        public int PackageId {  get; set; }
-        public int Rating {  get; set; }
-        public string Comment {  get; set; }
+        public int UserID { get; set; }
+        public int PackageID { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
         public DateTime TimeStamp { get; set; }
+
     }
 }
