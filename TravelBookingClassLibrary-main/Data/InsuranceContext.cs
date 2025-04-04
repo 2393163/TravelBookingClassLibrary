df94efa8133d2a1a5b3142cff689b0c7a62b8cc2
2392823 <Sabura.M@cognizant.com>
2025-04-04 17:46:23 +0530

    Add files via upload

---
+++ TravelBookingClassLibrary-main/Data/InsuranceContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InsuranceClass.Entity;
+using Microsoft.EntityFrameworkCore;
+//using TravelBookingClassLibrary.Entity;
+namespace InsuranceClass.Data
+{
+    public class InsuranceContext : DbContext
+    {
+       
+
+        //public DbSet<User> Users { get; set; }
+        public DbSet<Insurance> Insurances { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Replace connection string with your actual database details
+            optionsBuilder.UseSqlServer("Data Source=LTIN593162;Initial Catalog=InsuranceDatabases;Integrated Security=True;TrustServerCertificate=true");
+        }
+    }
+
+}
+    
+
