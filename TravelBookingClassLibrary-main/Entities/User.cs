e52417da51cb4e203906ba9fb0ee21bd60ba6eea
SairamThankari-1 <sairam.thankari@cognizant.com>
2025-04-04 08:28:43 +0530

    Add files via upload

--- TravelBookingClassLibrary-main/Entities/User.cs
+++ TravelBookingClassLibrary-main/Entities/User.cs
@@ -2,19 +2,43 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace TravelBookingClassLibrary.Entity
+namespace TravelBookingClassLibrary.Entities
 {
     public class User
     {
         [Key]
-        public int UserID { get; set; }
-        
+        public long UserID { get; set; }
 
-        // Navigation properties to link with related entities
-        public required List<Insurance> Insurances { get; set; }
-        public required List<Assistance> AssistanceRequests { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; } // "Admin", "TravelAgent", "Customer"
+
+        public string ContactNumber { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return $"Id:{UserID}, Name:{Name}, Email:{Email},Role:{Role} ";
+        }
     }
+
 }
+
