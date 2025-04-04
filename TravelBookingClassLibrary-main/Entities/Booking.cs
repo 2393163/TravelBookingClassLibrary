f97964c5fb8c5af3334826e6881da832e9aaf735
2392823 <Sabura.M@cognizant.com>
2025-04-04 17:45:49 +0530

    Update Booking.cs

--- TravelBookingClassLibrary-main/Entities/Booking.cs
+++ TravelBookingClassLibrary-main/Entities/Booking.cs
@@ -1,18 +1,22 @@
-namespace TravelBookingClassLibrary
-{    
-        public class Booking
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceClass.Entity
+{
+     public class Booking
     {
-        
-            public int BookingID { get; set; }
-            public int UserID { get; set; }
-            public int PackageID { get; set; }
-            public DateTime StartDate { get; set; }
-            public DateTime EndDate { get; set; }
+        public int BookingID { get; set; }
+        public int UserID { get; set; }
+        public int PackageID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
 
-            public string Status { get; set; } = "Active";
-            public int PaymentID { get; set; }
-            //public Payment Payment { get; set; } // Navigation property
-
+       
+        public int PaymentID { get; set; }
+        public int? InsuranceID { get; set; }
+        public Insurance? Insurance{ get; set; }
     }
 }
-
