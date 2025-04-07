34f33931ef05df109343c668714d1d7a7b1faa22
2392823 <Sabura.M@cognizant.com>
2025-04-07 12:25:50 +0530

    Add files via upload

---
+++ TravelBookingClassLibrary-main/Data/AssistanceDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TravelAssistance.Entity;
+//using TravelBookingClassLibrary.Entity;
+namespace TravelAssistance.Data
+{
+    public class AssistanceContext : DbContext
+    {
+
+
+        //public DbSet<User> Users { get; set; }
+        public DbSet<Assistance> Assistances { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Replace connection string with your actual database details
+            optionsBuilder.UseSqlServer("Data Source=LTIN593162;Initial Catalog=AssistanceDatabases;Integrated Security=True;TrustServerCertificate=true");
+        }
+    }
+
+}
+
+
