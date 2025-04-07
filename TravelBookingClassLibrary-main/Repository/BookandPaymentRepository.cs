7eee59dca05d9d6641f8585c15cb2c543a663395
2392823 <Sabura.M@cognizant.com>
2025-04-07 14:41:30 +0530

    Update BookandPaymentRepository.cs

--- TravelBookingClassLibrary-main/Repository/BookandPaymentRepository.cs
+++ TravelBookingClassLibrary-main/Repository/BookandPaymentRepository.cs
@@ -1,69 +1,168 @@
-using Microsoft.EntityFrameworkCore;
-using TravelBookingClassLibrary;
+using InsuranceClass.Entity;
 using System;
-public class BookandPaymentRepository
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InsuranceClass.Data;
+namespace InsuranceClass.Repository
 {
-    public void AddBooking(Booking booking)
+    public class BookingRepository
     {
-        using(var context = new BookingPaymentContext())
-        {
-            context.Bookings.Add(booking);
-            context.SaveChanges();
-        }
-    }
-    public List<Booking> GetAllUsers()
-    {
-        using (var context = new BookingPaymentContext())
-        {
-            var bookings = context.Bookings.ToList<Booking>();
-            return bookings;
-        }
-    }
-    public List<Booking> GetBookingsByBookingID(int BookingID)
-    {
-        using (var context = new BookingPaymentContext())
-        {
-            var bookings = context.Bookings.Where(a => a.BookingID == BookingID).ToList();
-            return bookings;
-        }
-    }
 
-    public  void updateBooking(int BookingID, DateTime StartDate)
-    {
-        using (var context = new BookingPaymentContext())
+        public void AddBooking(Booking booking)
+
         {
-            var user = context.Bookings.Find(BookingID);
-            if(user != null)
+
+            using (var context = new InsuranceContext())
+
             {
-                user.StartDate = StartDate;
+
+                context.Bookings.Add(booking);
+
                 context.SaveChanges();
-            }
-        }
-    }
-    //public static void cancelBooking(int BookingID)
-    //{
-    //    using (var context = new BookingPaymentContext())
-    //    {
-    //        var booking = context.Bookings.Find(BookingID);
-    //        if (booking != null)
-    //        {
-    //            booking.Status = "Cancelled";
-    //            context.SaveChanges();
-    //        }
-    //    }
-    //}
-    public void DeleteBooking(int BookingID)
-    {
-        using(var dbContext = new BookingPaymentContext())
-        {
-            var user = dbContext.Bookings.Find(BookingID);
-            if(user != null)
-            {
-                dbContext.Bookings.Remove(user);
-                dbContext.SaveChanges();
-            }
-        }
-    }
 
+            }
+
+        }
+
+        public List<Booking> GetAllBookings()
+
+        {
+
+            using (var context = new InsuranceContext())
+
+            {
+
+                var insurances = context.Bookings.ToList();
+
+                return insurances;
+
+            }
+        }
+
+
+        public void updateBooking(int BookingID, DateTime StartDate)
+        {
+            using (var context = new InsuranceContext())
+            {
+                var user = context.Bookings.Find(BookingID);
+                if (user != null)
+                {
+                    user.StartDate = StartDate;
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void DeleteBooking(int BookingID)
+        {
+            using (var dbContext = new InsuranceContext())
+            {
+                var user = dbContext.Bookings.Find(BookingID);
+                if (user != null)
+                {
+                    dbContext.Bookings.Remove(user);
+                    dbContext.SaveChanges();
+                }
+            }
+        }
+        //public Insurance GetInsuranceByUserID(int userID)
+        //{
+        //    using (var context = new InsuranceandAssistanceContext())
+        //    {
+        //        return context.Insurances.FirstOrDefault(i => i.UserID == userID);
+        //    }
+        //}
+
+        public List<Insurance> GetInsuranceByProvider(string provider)
+        {
+            using (var context = new InsuranceContext())
+            {
+                return context.Insurances.Where(i => i.Provider.Contains(provider)).ToList();
+            }
+        }
+
+        //public List<Insurance> GetInsurancesByDateRange(DateTime startDate, DateTime endDate)
+        //{
+        //    using (var context = new InsuranceandAssistanceContext())
+        //    {
+        //        return context.Insurances
+        //            .Where(i => i.PurchaseDate >= startDate && i.PurchaseDate <= endDate)
+        //            .ToList();
+        //    }
+        //}
+        //public List<Insurance> GetInsuranceByPackage(string package)
+        //{
+        //    using (var context = new InsuranceandAssistanceContext())
+        //    {
+        //        return context.Insurances.Where(i => i.PackageID.Contains(package)).ToList();
+        //    }
+        //}
+        public int GetTotalInsuranceCount()
+        {
+            using (var context = new InsuranceContext())
+            {
+                return context.Insurances.Count();
+            }
+        }
+
+
+       
+        
+            public void AssignInsuranceAndUpdateInsuranceTable(Booking booking)
+            {
+                using (var context = new InsuranceContext())
+                {
+                    // Calculate the difference between StartDate and EndDate
+                    var daysDifference = (booking.EndDate - booking.StartDate)?.Days;
+
+                    if (daysDifference == null)
+                        throw new ArgumentException("EndDate or StartDate cannot be null.");
+
+                    // Select the appropriate insurance based on the difference
+                    Insurance insurance = null;
+
+                    if (daysDifference == 3)
+                    {
+                        insurance = context.Insurances.FirstOrDefault(i => i.InsuranceID == 1);
+                    }
+                    else if (daysDifference == 5)
+                    {
+                        insurance = context.Insurances.FirstOrDefault(i => i.InsuranceID == 2);
+                    }
+                    else if (daysDifference == 10)
+                    {
+                        insurance = context.Insurances.FirstOrDefault(i => i.InsuranceID == 3);
+                    }
+
+                    // Ensure the appropriate insurance exists
+                    if (insurance == null)
+                    {
+                        throw new Exception($"No matching insurance found for {daysDifference} days.");
+                    }
+
+                    // Assign the insurance ID to the booking
+                    booking.InsuranceID = insurance.InsuranceID;
+
+                    // Add the booking to the insurance's list of bookings
+                    if (insurance.Bookings == null)
+                    {
+                        insurance.Bookings = new List<Booking>();
+                    }
+                    insurance.Bookings.Add(booking);
+
+                    // Save the changes to the database
+                    context.Bookings.Add(booking); // Add the booking to the Bookings table
+                    context.Insurances.Update(insurance); // Update the Insurance table with the booking list
+                    context.SaveChanges();
+                }
+            }
+        
+
+
+
+
+    }
 }
 
