0001a0b5deb84c2c85c53009252356eecd9aea30
2392823 <Sabura.M@cognizant.com>
2025-04-07 12:24:52 +0530

    Add files via upload

---
+++ TravelBookingClassLibrary-main/Entities/Assistance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAssistance.Entity
+{
+    public class Assistance
+    {
+        [Key]
+        public int RequestID { get; set; } // Auto-increment primary key
+        public int UserID { get; set; }
+        public string IssueDescription { get; set; }
+        public string? Status { get; set; } = "Active"; // Default: "Active"
+        public int? ResolutionTime { get; set; }
+    }
+}
